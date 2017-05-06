9085bfa867608c31f02bee303f27d56809d45b57
LogicAppSourceIO <logicappsource@gmail.com>
2017-05-06 17:48:31 +0200

    Basic

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,3 +1,11 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2017. MIT licensed.
+ */
 (function ($, window, document, undefined) {
 
   'use strict';
