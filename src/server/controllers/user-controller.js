29d5bb59db34c30f5230fcc11636cb3a723b825a
LogicAppSourceIO <logicappsource@gmail.com>
2017-06-12 17:14:11 +0200

    Web token + Auth for logging in

--- src/server/controllers/user-controller.js
+++ src/server/controllers/user-controller.js
@@ -1,6 +1,8 @@
 var db = require('../database/database'); 
 var bcrypt = require('bcryptjs'); 
 var salt = bcrypt.genSaltSync(5);
+var jwt = require('jsonwebtoken');
+
 
 
 module.exports.createUser = function(req, res) {
@@ -12,6 +14,35 @@ module.exports.createUser = function(req, res) {
             res.status(200).send("User was successfully created.");
     }).catch(function(err) {
             res.status(500).send("User was not created - Failed ");
+    })
+}
+
+module.exports.logIn = function(req, res) {
+    var submittedPassword = req.body.password; 
+    var query = "SELECT * FROM users WHERE username='" + req.body.loginName + "' OR email='" + req.body.loginName + "'";
+
+    var testQuery = "SELECT * FROM users"; 
+
+    db.query(query).spread(function(result, metadata) {
+        if(result.length > 0) {
+           var userData = result[0];
+           var isVerified = bcrypt.compareSync(submittedPassword, userData.user_password);
+
+           var token = jwt.sign(userData, process.env.SECRET, {
+                    expiresIn: 60*60*24
+           })
+
+            if (isVerified) {
+                //User Authenticated - Apply token 
+                      res.json({
+                    userData: userData  
+                })
+            } else {
+                res.status(400).send("user enter the wrong password "); 
+                    }           
+        }
+        }).catch(function(err) {
+            res.status(500).send("unable to process the query. ");  
     })
 }
 
--- src/server/server.js
+++ src/server/server.js
@@ -2,18 +2,20 @@ var express = require('express');
 var app = express(); 
 var bodyParser = require('body-parser'); 
 var db = require('./database/database');
+var jwt = require('jsonwebtoken'); 
+
+process.env.SECRET = "logiccryptonite"; 
 
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended: true}));
-
 
 //Controllers 
 var userController = require('./controllers/user-controller'); 
 
 //Routes 
 app.post('/api/user/create', userController.createUser); 
-
+app.post('/api/user/login', userController.logIn);
 
 
 db.sync().then(function() {
