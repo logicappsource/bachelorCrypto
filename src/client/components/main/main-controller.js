759c34b839ecbb2d79c3f1101827518e3eaaa75d
LogicAppSourceIO <logicappsource@gmail.com>
2017-06-23 16:48:59 +0200

    Main Minor bug fix

--- src/client/components/main/main-controller.js
+++ src/client/components/main/main-controller.js
@@ -2,7 +2,18 @@
     angular.module('app')
     .controller('mainCtrl', ['$scope', '$state', '$http', 'userSvc', function($scope, $state, $http, userSvc) {
             $scope.userData = userSvc.user; 
-
             console.log(userSvc.token);
+        
+            $http({
+                method: "GET",
+                url: '/secure-api/user/get-friends',
+                headers: {
+                    'auth-token': userSvc.token
+                }
+            }).then(function(response) {
+                console.log(response);
+            }, function(err) {
+                console.err(err);
+            })
     }]);
 })(window, window.angular)
